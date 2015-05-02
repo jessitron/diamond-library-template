0e72ab88ec581eb26286cf10bc07989f9a791472
Jessica Kerr <jessitron@outpace.com>
2015-05-02 17:41:29 +0000

    Remove -SNAPSHOT, so that local installs are testable

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diamond-library/lein-template "0.1.0-SNAPSHOT"
+(defproject diamond-library/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
