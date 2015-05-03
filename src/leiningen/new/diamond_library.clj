3d713201013e305b5a93643264d2fe442ed5796f
Jessica Kerr <jessitron@outpace.com>
2015-05-03 16:37:46 +0000

    Make it work: two simple files

--- src/leiningen/new/diamond_library.clj
+++ src/leiningen/new/diamond_library.clj
@@ -8,14 +8,14 @@
   "A whole structure: impl, schema, gen, spec"
   [name]
   (let [data {:name name
+              :schema-project (str name "-schema")
+              :gen-project (str name "-gen")
+              :impl-project (str name "-impl")
+              :specs-project (str name "-spec")
               :sanitized (sanitize name)
               :year (year)}]
     (main/info "Generating four fresh projects for your very much library implementation")
     (->files data
-             ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
-             ["project.clj" (render "project.clj" data)]
-             ["README.md" (render "README.md" data)]
-             ["LICENSE" (render "LICENSE" data)]
+             ["README.md" (render "toplevel-README.md" data)]
              [".gitignore" (render "gitignore" data)]
-             ["test/{{sanitized}}/core_test.clj" (render "test.clj" data)]
              )))
---
+++ src/leiningen/new/diamond_library/toplevel-README.md
@@ -0,0 +1,17 @@
+{{name}}
+========
+
+PUT OVERALL LIBRARY DESCRIPTION HERE
+
+## Usage
+
+this should show how to use {{impl-project}} in src dep and {{gen-project}} in test dep.
+
+this should probably link to something in the specs project for API descriptions.
+
+## License
+
+Copyright © {{year}} YOUR NAME HERE
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
